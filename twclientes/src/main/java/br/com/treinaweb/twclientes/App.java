2c3c71733b579016862caa2ebf359dd4f7e78ddf
denis <denisinfo@usp.br>
2021-06-18 00:20:02 -0300

    4.1 Criando projeto Spring Boot com o Maven

--- twclientes/src/main/java/br/com/treinaweb/twclientes/App.java
+++ twclientes/src/main/java/br/com/treinaweb/twclientes/App.java
@@ -1,13 +1,17 @@
 package br.com.treinaweb.twclientes;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   //primeira classe a ser executada
+        SpringApplication.run(App.class, args);
     }
 }
